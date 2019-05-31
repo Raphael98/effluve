4a4065207f4c3febc3adc23ca08f621fa912d0d6
Raphael A. Alves <raphael.andrade98@outlook.com>
2019-05-31 02:27:23 -0300

    commit

--- src/main/java/controller/product/Manager.java
+++ src/main/java/controller/product/Manager.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/productManaging")
+@WebServlet(name = "productManager", urlPatterns = {"/productManaging"})
 public class Manager extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
