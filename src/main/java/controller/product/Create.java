99fa5e65fd82eadb999bb34a4c1578f08f977eb9
Raphael Alves <raphael.alves@visie.com.br>
2019-05-31 10:51:35 -0300

    asdad

--- src/main/java/controller/product/Create.java
+++ src/main/java/controller/product/Create.java
@@ -22,7 +22,6 @@ public class Create extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Product product = new Product(req.getParameter("name"),req.getParameter("price"),req.getParameter("brand"),req.getParameter("description"),req.getParameter("gender"));
-		product.setId(Integer.parseInt(req.getParameter("id")));
 		new ProductDAO().add(product);
 		req.setAttribute("success", true);
 		req.getRequestDispatcher("/WEB-INF/jsp/product/new.jsp").forward(req, resp);
